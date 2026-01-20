36a9bec488a66e486b54d44d70ef3efbc6551fbd
Juan Diego Alvarez Arteaga <jdalvarezarteaga@gmail.com>
2026-01-19 18:42:59 -0600

    Add Javadoc comments to Carrito class
    Added documentation comments to the Carrito class and its methods.

--- src/edu/juan/agencia/data/Carrito.java
+++ src/edu/juan/agencia/data/Carrito.java
@@ -1,13 +1,34 @@
 package edu.juan.agencia.data;
 
+/**
+ *
+ * Esta clase representa un carrito de compra.
+ * Se encarga de almacenar el auto seleccionado
+ * y generar el ticket de compra.
+ */
 public class Carrito {
 
+    /**
+     *
+     * Auto seleccionado para la compra
+     */
     private Auto auto;
 
+    /**
+     *
+     * Este método agrega un auto al carrito
+     * @param auto auto seleccionado
+     * @return
+     */
     public void agregarAuto(Auto auto) {
         this.auto = auto;
     }
 
+    /**
+     *
+     * Este método muestra el ticket de compra en consola
+     * @return
+     */
     public void mostrarTicket() {
         if (auto == null) {
             System.out.println("❌ No hay compra registrada");
