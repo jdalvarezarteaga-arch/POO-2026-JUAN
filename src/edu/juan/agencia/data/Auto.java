6a50b285978424be8d1d83c2659e08f8a80f2669
Juan Diego Alvarez Arteaga <jdalvarezarteaga@gmail.com>
2026-01-19 18:40:02 -0600

    Add Javadoc comments to Auto class
    Added documentation comments for class and methods.

--- src/edu/juan/agencia/data/Auto.java
+++ src/edu/juan/agencia/data/Auto.java
@@ -1,15 +1,65 @@
 package edu.juan.agencia.data;
 
+/**
+ *
+ * Esta clase representa un automóvil de la agencia.
+ * Almacena todas las características y el precio del auto.
+ */
 public class Auto {
 
+    /**
+     *
+     * Marca del automóvil
+     */
     public String marca;
+
+    /**
+     *
+     * Año del automóvil
+     */
     public int anio;
+
+    /**
+     *
+     * Acabado del automóvil
+     */
     public String acabado;
+
+    /**
+     *
+     * Tipo de llantas del automóvil
+     */
     public String llantas;
+
+    /**
+     *
+     * Color del automóvil
+     */
     public String color;
+
+    /**
+     *
+     * Tipo de motor del automóvil
+     */
     public String motor;
+
+    /**
+     *
+     * Precio del automóvil
+     */
     public double precio;
 
+    /**
+     *
+     * Constructor del automóvil
+     * @param marca marca del auto
+     * @param anio año del auto
+     * @param acabado tipo de acabado
+     * @param llantas tipo de llantas
+     * @param color color del auto
+     * @param motor tipo de motor
+     * @param precio precio del automóvil
+     */
     public Auto(String marca, int anio, String acabado,
                 String llantas, String color, String motor, double precio) {
         this.marca = marca;
@@ -21,6 +71,11 @@ public class Auto {
         this.precio = precio;
     }
 
+    /**
+     *
+     * Este método muestra en consola la información del automóvil
+     * @return
+     */
     public void mostrar() {
         System.out.println("Auto: " + marca);
         System.out.println("Año: " + anio);
